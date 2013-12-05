5146173016c56961268737a87651a318e1cdc52c
ungpale <ungpale@naver.com>
2013-12-05 08:41:35 -0800

    library_new.html 브레이브이노베이션에서 사용목적에 맞도록 수정.

--- coding/@guide/js/common.js
+++ coding/@guide/js/common.js
@@ -1,8 +1,22 @@
 $(document).ready(function() {
 	//Lnb
 	//조건 연산 (조건)?(조건이 참일 때의 값) : (조건이 거짓일 때의 값)
-	$(".lnb li.dropdown > a").click(function () {
-        $(this).parent("li").hasClass("open") ? $(this).parent("li").removeClass("open") : $(this).parent("li").addClass("open");
-        return !1
+	// $(".lnb li.dropdown > a").click(function () {
+ //        $(this).parent("li").hasClass("open") ? $(this).parent("li").removeClass("open") : $(this).parent("li").addClass("open");
+ //        return !1
+ //    });
+
+    $('.lnb > li > a').on('click',function (){
+    	var depth1 = $('.lnb > li > a')
+    	var lst = $(this).parent('li');
+
+    	if (lst.hasClass('dropdown')) {
+    		lst.siblings('li').removeClass('open');
+    		lst.toggleClass('open');
+    	} else {
+    		lst.siblings('li').removeClass('open');
+    		lst.addClass('open');
+    	}
+    	
     });
 });
