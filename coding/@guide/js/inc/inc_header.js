f976afb37fdaa0948e4639494efd9f9419b31f1a
ungpale <ungpale@naver.com>
2014-01-06 00:42:58 -0800

    이노마인드에 맞게 잠깐 수정했음. 다시 원복하여 송우진껄로 만들어야 함.

--- coding/@guide/js/inc/inc_header.js
+++ coding/@guide/js/inc/inc_header.js
@@ -1,13 +1,13 @@
 var header = '';
 
 header +=	'<div id="header">'
-header +=		'<h1><span class="corner">브레이브이노베이션 퍼블리싱 페이지 리스트</span></h1>'
+header +=		'<h1><span class="corner">이노마인드 퍼블리싱 페이지 리스트</span></h1>'
 header +=		'<!-- <ul>'
 header +=			'<li><a href="resume_a.html" class="on">프로젝트 기본항목</a></li>'
 header +=			'<li><a href="library_new.html">라이브러리</a></li>'
 header +=			'<li><a href="pagelist.html">페이지리스트</a></li>'
 header +=		'</ul> -->'
-header +=		'<div class="corner" style="position:absolute;padding:6px;right:20px;top:15px;background-color:#fff;"><img src="images/company.gif" alt="브레이브이노베이션 로고" /></div>'
+header +=		'<div style="position:absolute;padding:6px;right:20px;top:12px;"><img src="images/logo_innomind.png" alt="이노마인드 로고" style="width:150px;" /></div>'
 header +=	'</div>'
 
 document.write(header);
